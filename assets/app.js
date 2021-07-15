9d486dc2aa7d1607e33d07855f02795d12d36a8e
benjamin nsingi <b.nsingi07@gmail.com>
2021-07-15 23:03:32 +0200

    feat: add(front: homepage)

--- assets/app.js
+++ assets/app.js
@@ -1,54 +1,16 @@
 import Places from 'places.js'
-import Filter from "./js/filter";
 import 'select2';
 import $ from "jquery";
 import './js/admin';
 import './js/picture';
-
-new Filter(document.querySelector('.js-filter'))
+import './js/navbar';
+import './js/slider';
 
 // start the Stimulus application
-import noUSlider from 'nouislider';
-import 'nouislider/dist/nouislider.css'
+import '@fortawesome/fontawesome-free/css/all.css';
+import 'boxicons/css/boxicons.min.css';
 import 'select2/dist/css/select2.css'
-import './css/app.css';
-import './bootstrap';
-
-import 'bootstrap/dist/css/bootstrap.css';
-
-const slider = document.getElementById('price-slider');
-
-/**
- * The price filter
- *
- * This system will allow the user to change the minimum and maximum price by simple drag and drop.
- */
-if (slider) {
-    const min = document.getElementById('min')
-    const max = document.getElementById('max')
-    const minValue = Math.floor(parseInt(slider.dataset.min, 10) / 10) * 10
-    const maxValue = Math.ceil(parseInt(slider.dataset.max, 10) / 10) * 10
-    const range = noUSlider.create(slider, {
-        start: [min.value || minValue, max.value || maxValue],
-        connect: true,
-        step: 10,
-        range: {
-            'min': minValue,
-            'max': maxValue
-        }
-    })
-    range.on('slide', function (values,handleNumber) {
-        if (handleNumber === 0 ) {
-            min.value = Math.round(values[0])
-        }
-        if (handleNumber === 1) {
-            max.value = Math.round(values[1])
-        }
-    })
-    range.on('end', function (values,handleNumber){
-        min.dispatchEvent(new Event('change'))
-    })
-}
+import './css/app.scss';
 
 $(document).ready(function() { $("select").select2(); });
 
---
+++ assets/js/slider.js
@@ -0,0 +1,25 @@
+// import Swiper JS
+import 'swiper/swiper-bundle';
+// import Swiper styles
+import 'swiper/swiper-bundle.css';
+import Swiper from "swiper";
+
+const swiper = new Swiper(".heroslider", {
+    spaceBetween: 30,
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    effect: "fade",
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
